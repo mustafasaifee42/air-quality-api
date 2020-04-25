6376c53bfa8495e91cc77471ea499c859135c7f6
mustafasaifee42 <saifee.mustafa@gmail.com>
2020-04-25 20:13:48 +0300

    adding homepage

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // create an express app
 const express = require("express");
-const DocumentationRouter = require("./routes/documentation");
 const listRouter = require("./routes/list");
 const HourDataRouter = require("./routes/hourly-data");
 const DailyAverageDataRouter = require("./routes/daily-average");
@@ -10,9 +9,13 @@ const app = express();
 // use the express-static middleware
 app.use(express.static("public"));
 
-// define the first route\
+// define the first route
+app.get("/", function (req, res) {
+  res.send(
+    "<h2>Unofficial API for fetching air quality data from Berkley Earth (PM 2.5 and PM 10 levels)</h2> <br/><br/>Documentation can be found <a href='https://github.com/mustafasaifee42/air-quality-api' target='_blank'>here</a> on github"
+  );
+});
 
-app.use("/", DocumentationRouter);
 app.use("/list", listRouter);
 app.use("/air-quality/hourly-data", HourDataRouter);
 app.use("/air-quality/daily-average", DailyAverageDataRouter);
