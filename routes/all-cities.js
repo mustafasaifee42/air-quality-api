f168f9ebbdf7dc6f965ee151033915127684ad4f
mustafasaifee42 <saifee.mustafa@gmail.com>
2020-11-23 23:01:29 +0200

    day to hour

--- routes/all-cities.js
+++ routes/all-cities.js
@@ -14,7 +14,7 @@ router.get("/", function (req, res, next) {
             country:d[0],
             region:d[1],
             'PM2.5': {
-              "Last Day":d[3],
+              "Last Hour":d[3],
               "Last Day":d[4],
               "Last 2 Days":d[5],
               "Last 3 Days":d[6],
